ee6d2628134f0da00e3ff31a077099f87d237eaf
PauloK3tchup <passandoalimpo13@gmail.com>
2023-03-07 16:35:52 -0300

    bruh

---
+++ src/components/Counter.js
@@ -0,0 +1,43 @@
+import { Component } from "react";
+import { StyleSheet, Text, Button, View } from "react-native";
+
+export default class Counter extends Component {
+  state = {
+    count: 0,
+  };
+  incrementar() {
+    this.setState({ count: this.state.count + 1 });
+  }
+  subtrair() {
+    if (this.state.count > 0) {
+      this.setState({ count: this.state.count - 1 });
+    }
+  }
+  render() {
+    return (
+      <View style={styles.contador}>
+        <Text style={styles.textoComFundinho}>
+          FUCKS GIVEN: {this.state.count}
+        </Text>
+        <Button onPress={() => this.incrementar()} title="+💀" />
+        <Button onPress={() => this.subtrair()} title="-💀" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  textoComFundinho: {
+    fontWeight: "bold",
+    color: "white",
+    margin: 10,
+  },
+  contador: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "crimson",
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+  },
+});
