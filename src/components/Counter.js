cb63555dd54e1ead5bcf27863ba5f25ea4987eab
PauloK3tchup <passandoalimpo13@gmail.com>
2023-03-08 11:48:05 -0300

    muito mais poggersso

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,19 +1,31 @@
 import { Component } from "react";
-import { StyleSheet, Text, Button, View } from "react-native";
+import { StyleSheet, Text, Button, View, TextInput } from "react-native";
 
 export default class Counter extends Component {
   state = {
     count: 0,
+    step: 2,
   };
   incrementar() {
-    if (this.state.count < 50) {
-      this.setState({ count: this.state.count + 1 });
+    if (this.state.count < 500) {
+      this.setState({ count: this.state.count + this.state.step });
+    } else {
+      if (this.state.count > 500) {
+        this.setState({ count: 500 });
+      }
     }
   }
   subtrair() {
     if (this.state.count > 0) {
-      this.setState({ count: this.state.count - 1 });
+      this.setState({ count: this.state.count - this.state.step });
+    } else {
+      if (this.state.count < 0) {
+        this.setState({ count: 0 });
+      }
     }
+  }
+  alteraStep(e) {
+    this.setState({ step: parseInt(e) });
   }
   render() {
     return (
@@ -23,6 +35,14 @@ export default class Counter extends Component {
         </Text>
         <Button onPress={() => this.incrementar()} title="+💀" />
         <Button onPress={() => this.subtrair()} title="-💀" />
+        <TextInput
+          style={styles.inputComFundinho}
+          value={this.state.step}
+          onChangeText={(e) => this.alteraStep(e)}
+          inputMode="numeric"
+          keyboardType="numeric"
+        />
+        <Text style={styles.textoComFundinho}>Step: {this.state.step}</Text>
       </View>
     );
   }
@@ -33,6 +53,14 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "white",
     margin: 10,
+  },
+  inputComFundinho: {
+    fontWeight: "bold",
+    color: "white",
+    margin: 10,
+    backgroundColor: "#b50021",
+    padding: 15,
+    borderRadius: 5,
   },
   contador: {
     flexDirection: "row",
